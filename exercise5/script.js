c7e02cbcbbfb84a54a499a8c6ba63bae96a74e2e
“Prabhjyot15” <bamrahprabhjyot@gmail.com>
2023-11-13 00:21:45 -0800

    Content updates and code cleanup

--- exercise5/script.js
+++ exercise5/script.js
@@ -1,13 +1,16 @@
 function setupTabs() {
     document.querySelectorAll(".tabs__button").forEach((button) => {
         button.addEventListener("click", () => {
+            //get references to elements
             const sideBar = button.parentElement;
             const tabsContainer = sideBar.parentElement;
-            // identifying tab numbers
+            // identifying tab numbers and gets the number that is to be activated
             const tabNumber = button.dataset.forTab;
             // activation of a given tab
             const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabNumber}"]`);
 
+
+            //deactivate all other tabs
             sideBar.querySelectorAll(".tabs__button").forEach((button) => {
                 button.classList.remove("tabs__button--active");
             });
@@ -22,24 +25,7 @@ function setupTabs() {
     });
 }
 
+//Execute everything after DOM is loaded as We need access to the DOM elements
 document.addEventListener("DOMContentLoaded", () => {
     setupTabs();
 });
-
-// const firstButton = document.getElementsByTagName('button')[0]
-// const sideBar = firstButton.parentElement;
-// const tabsContainer = sideBar.parentElement;
-
-// const tabNumber = button.dataset.forTab;
-// // activation of a given tab
-// const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabNumber}"]`);
-// sideBar.querySelectorAll(".tabs__button").forEach((button) => {
-//     button.classList.remove("tabs__button--active");
-// });
-
-// tabsContainer.querySelectorAll(".tabs__content").forEach((tab) => {
-//     tab.classList.remove("tabs__content--active");
-// });
-
-// button.classList.add("tabs__button--active");
-// tabToActivate.classList.add("tabs__content--active");
